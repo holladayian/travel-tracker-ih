65053db7e8818b576eb15cbcec8fd1606c8a45d2
Ian Holladay <holladayian@gmail.com>
2020-09-20 10:28:57 -0600

    Add 'request' section

--- src/fetch.js
+++ src/fetch.js
@@ -1,28 +1,28 @@
 import fetchSetter from './index.js';
 
 let fetcher = {
- fetchUser(id) {
-     const fetchedUser = `https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers/${id}`
-     const promise =  fetch(fetchedUser)
-     .then(response => response.json())
-     return promise
-     .catch(err => console.log('err', err))
-},
-
- fetchTripsForAUser() {
-    const fetchedAllTrips = 'https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips'
-    const promise = fetch(fetchedAllTrips)
-    .then(response => response.json())
-    return promise
-    .catch(err => console.log('err', err))
+    fetchUser(id) {
+        const fetchedUser = `https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers/${id}`
+        const promise =  fetch(fetchedUser)
+            .then(response => response.json())
+        return promise
+            .catch(err => console.log('err', err))
     },
+
+    fetchTripsForAUser() {
+        const fetchedAllTrips = 'https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips'
+        const promise = fetch(fetchedAllTrips)
+            .then(response => response.json())
+        return promise
+            .catch(err => console.log('err', err))
+        },
 
     fetchDestination() {
         const fetchedAllDestinations = 'https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/destinations/destinations'
         const promise = fetch(fetchedAllDestinations)
-        .then(response => response.json())
+            .then(response => response.json())
         return promise
-        .catch(err => console.log('err', err))
+            .catch(err => console.log('err', err))
     }
 }
 
